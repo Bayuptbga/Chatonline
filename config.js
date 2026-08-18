91e28bd2948e09248f289d9888fe2cb12ac67756
Bayuptbga <114854983+Bayuptbga@users.noreply.github.com>
2026-08-19 00:17:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   API_KEY: "AIzaSyBxmHrrGOA_TseA3OtthWtXkjsda_vVtfQ",
 
   // ID folder Google Drive yang berisi video-video
-  FOLDER_ID: "1Zz47e3-ewXqt1y3qvEgBQWnug1LLt2fA",
+  FOLDER_ID: "1uXPiDOxSF2VWF20QQnnAmHXSxp6iaNdm",
 
   // Jumlah video yang diambil per load
   PAGE_SIZE: 10,
