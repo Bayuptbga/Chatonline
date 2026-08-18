3689fca4f7b81f446a7980f61c0e137788973e5a
Bayuptbga <114854983+Bayuptbga@users.noreply.github.com>
2026-08-19 00:29:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // Dapatkan dari Google Cloud Console > APIs & Services > Credentials
   // Aktifkan "Google Drive API" dulu, lalu buat API Key
   // WAJIB dibatasi (restrict) ke domain GitHub Pages kamu di halaman kredensial
-  API_KEY: "AIzaSyBxmHrrGOA_TseA3OtthWtXkjsda_vVtfQ",
+  API_KEY: "19lhp5W0hbq_Iv2IWol9NA7lAdZCgNyEy",
 
   // ID folder Google Drive yang berisi video-video
   FOLDER_ID: "1uXPiDOxSF2VWF20QQnnAmHXSxp6iaNdm",
