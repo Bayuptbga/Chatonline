e8ec5537f78a65b5afc13e737404a33c8029ee14
Bayuptbga <114854983+Bayuptbga@users.noreply.github.com>
2026-08-19 09:42:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@
 const CONFIG = {
   API_KEY: "AIzaSyBxmHrrGOA_TseA3OtthWtXkjsda_vVtfQ",
 
-  // 5 Folder ID yang sudah dimasukkan
+  // 5 Folder ID (sisa 1 aktif dulu untuk testing)
   FOLDER_IDS: [
     "12NSkb5iGK83stAkQnzEJNDcXZVt_Cd3d",
-    "1PWzl2KhbNvd8sBKPH0BiQUKBhB2a1-Uw",
-    "1w_FCtfUCGolrrXmCFZmFBo3iMNBiV4n2",
-    "1F1M-p9mVcQQsTo1qh9hK5-NxFNbPxp_q",
-    "122ALoQH6E4_rZCm08hFF3qcVpcXtmEaF"
+    // "1PWzl2KhbNvd8sBKPH0BiQUKBhB2a1-Uw",
+    // "1w_FCtfUCGolrrXmCFZmFBo3iMNBiV4n2",
+    // "1F1M-p9mVcQQsTo1qh9hK5-NxFNbPxp_q",
+    // "122ALoQH6E4_rZCm08hFF3qcVpcXtmEaF"
   ],
 
   // Jumlah video yang diambil per load
